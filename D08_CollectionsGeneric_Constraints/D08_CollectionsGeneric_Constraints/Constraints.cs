40e170f9594f91ef7436dc6f369fac86e9ef3d46
PJSM <pjsm.lesi@gmail.com>
2024-06-03 19:53:47 +0100

    Updated D07, Added D08

---
+++ D08_CollectionsGeneric_Constraints/D08_CollectionsGeneric_Constraints/Constraints.cs
@@ -0,0 +1,30 @@
+﻿namespace D08_CollectionsGeneric_Constraints
+{
+
+    internal class ReferenceTypeConstraint<T> where T : class
+    {
+
+        private T value;
+
+        public ReferenceTypeConstraint(T item)
+        {
+
+            value = item;
+
+        }
+
+        public T GetItem()
+        {
+
+            return value;
+
+        }
+
+        class Teste
+        {
+
+        }
+
+    }
+
+}
--- D08_CollectionsGeneric_Constraints/D08_CollectionsGeneric_Constraints/Properties/AssemblyInfo.cs
+++ D08_CollectionsGeneric_Constraints/D08_CollectionsGeneric_Constraints/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("D07_CollectionsGeneric_IEnumerable")]
+[assembly: AssemblyTitle("D08_CollectionsGeneric_Constraints")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("D07_CollectionsGeneric_IEnumerable")]
+[assembly: AssemblyProduct("D08_CollectionsGeneric_Constraints")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8846ffb4-f154-4601-bc58-2688d9931a77")]
+[assembly: Guid("2cc02468-d1ae-4237-afd7-a2f446972bcc")]
 
 // Version information for an assembly consists of the following four values:
 //
