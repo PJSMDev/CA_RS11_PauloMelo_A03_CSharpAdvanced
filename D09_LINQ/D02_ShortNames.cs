5f099e53888f0d1982362bb41e5e68b1a68bc3bb
PJSM <pjsm.lesi@gmail.com>
2024-06-03 22:45:34 +0100

    Added D09

---
+++ D09_LINQ/D02_ShortNames.cs
@@ -0,0 +1,77 @@
+﻿// Listar os nomes mais curtos
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D09_LINQ
+{
+
+    internal class D02_ShortNames
+    {
+
+        static List<string> listNames = new List<string>();
+
+        internal static void CreateListNames()
+        {
+
+            listNames.Add("Thiago");
+            listNames.Add("Rui");
+            listNames.Add("Manuela");
+            listNames.Add("Zé");
+            listNames.Add("Henrique");
+            listNames.Add("Ana");
+            listNames.Add("Sá");
+            listNames.Add("Francisco");
+            listNames.Add("Paula");
+
+        }
+
+        internal static void ShortNamesMethodSyntax()
+        {
+
+            // NOK: var shortNames = listNames.Where(l => l.Length == 2);
+            // Não está errado mas não é dinâmico e pode causar problemas no futuro, quando usar outras listas
+
+            var shortNames = listNames.Where(l1 => l1.Length == listNames.Min(l2 => l2.Length));
+            // equivalente a um SELECT dentro de outro SELECT
+
+            D00_Utility.Utility.WriteTitle("Short Names - Method Syntax");
+
+            D00_Utility.Utility.WriteMessage("\n");
+
+            foreach (var item in shortNames)
+            {
+                D00_Utility.Utility.WriteMessage(item, "", "\n");
+            }
+        }
+
+        internal static void ShortNamesQuerySyntax() 
+        {
+            
+            // Método a usar
+            #region v1: Com subquery e Min()
+            #endregion
+
+            // Método a usar
+            #region v2: Com subquery e First()
+            #endregion
+
+            // Método a evitar
+            #region v3: Sem subquery e Min() em method syntax
+            #endregion
+
+            // Minha versão atual -> Mudar!!!
+            int minLength = (from name in listNames
+                             select name.Length).Min();
+
+            var shortNames =
+                from name in listNames
+                where name.Length == minLength
+                select name;
+            
+        }
+    }
+}
---
+++ D09_LINQ/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using D00_Utility;
+
+namespace D09_LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Utility.SetUnicodeConsole();
+
+            #region D01 - Hello World
+
+            D01_HelloWorld.HelloWorldMethodSyntax();
+            D01_HelloWorld.HelloWorldQuerySyntax();
+
+            #endregion
+
+            Utility.WriteMessage("\n");
+
+            #region D02 - Short Names
+
+
+
+            #endregion
+
+            #region D03 - Numbers greater or equal to 10
+
+            D01_HelloWorld.HelloWorldQuerySyntax();     // CORRIGIR
+
+            #endregion
+
+            Utility.TerminateConsole();
+        }
+    }
+}
