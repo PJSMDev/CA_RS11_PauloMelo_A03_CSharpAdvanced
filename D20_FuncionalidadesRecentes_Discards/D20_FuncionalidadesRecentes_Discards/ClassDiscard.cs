0eede4edf962a9d6f3704a89eb18b070b8569be5
PJSM <pjsm.lesi@gmail.com>
2024-06-19 19:07:21 +0100

    Added: D17, D20, E04

---
+++ D20_FuncionalidadesRecentes_Discards/D20_FuncionalidadesRecentes_Discards/ClassDiscard.cs
@@ -0,0 +1,18 @@
+﻿namespace D20_FuncionalidadesRecentes_Discards
+{
+
+    internal static class Coordinates
+    {
+
+        public static (double latitude, double longitude) GetCoordinates(string city)
+        {
+
+            // Implementar o Google Maps...
+
+            return (10, 20);  // Devolve 1 tuple com 2 elementos
+
+        }
+
+    }
+
+}
---
+++ D20_FuncionalidadesRecentes_Discards/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D20_FuncionalidadesRecentes_Discards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
